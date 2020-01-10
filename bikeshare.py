a75cf7ee4fe3c059036cb0b9784788548399b3df
Nikolai Doinikov <nick.doynikov@gmail.com>
2020-01-10 14:50:04 +0100

    docs: Modify the comment section
    The word 'Args' changed to 'Arguments' in line 56 for clarity

--- bikeshare.py
+++ bikeshare.py
@@ -53,7 +53,7 @@ def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
-    Args:
+    Arguments:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
